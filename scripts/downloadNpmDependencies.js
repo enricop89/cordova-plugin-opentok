117f8a698b9abd7fbd259c03e7907cc2951236de
enricop89 <enrico.portolan@gmail.com>
2021-04-01 19:30:47 +0100

    - add q dep

--- scripts/downloadNpmDependencies.js
+++ scripts/downloadNpmDependencies.js
@@ -14,7 +14,7 @@
   // install the node dependencies for this project
   function install(context) {
     // set properties
-    var q = require("q");
+    var q = require('q');
     var async = new q.defer(); // eslint-disable-line
     var installFlagLocation = path.join(
       context.opts.projectRoot,
